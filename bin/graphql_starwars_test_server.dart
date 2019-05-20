01d352325d9db204187f0351164a62ba9ef8caf3
micimize <rosenthalm93@gmail.com>
2019-05-20 11:42:54 -0500

    making this thing work, kinda

--- bin/graphql_starwars_test_server.dart
+++ bin/graphql_starwars_test_server.dart
@@ -7,7 +7,7 @@ import 'package:args/args.dart';
 
 import 'package:graphql_starwars_test_server/src/pretty_logging.dart'
     show prettyLog;
-import 'package:graphql_starwars_test_server/star_wars.dart' as star_wars;
+import 'package:graphql_starwars_test_server/graphql_starwars_test_server.dart' as star_wars;
 
 final parser = ArgParser()
   ..addOption('host', defaultsTo: '127.0.0.1')
